7dc906179511aceea317497620e069d092dde3ec
Sourabh Mane <msourabh631@gmail.com>
2023-06-30 20:00:15 +0530

    Initial commit

---
+++ src/main/java/com/geekster/New_Patterns/NewPatternsApplication.java
@@ -0,0 +1,102 @@
+package com.geekster.New_Patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class NewPatternsApplication {
+
+	public static void main(String[] args) {
+        /*Que-1
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        Que-2
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        Que-3
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        Que-4
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        Que-5
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("      ");
+            }
+            for (int k = i; k <= n; k++) {
+                System.out.print("*");
+                System.out.print("     ");
+            }
+            System.out.println();
+        }
+
+        Que-6
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("        ");
+            }
+            for (int k = i; k <= n; k++) {
+                System.out.print("*");
+                System.out.print("     ");
+            }
+            System.out.println();
+        }
+
+        Que-7
+        int n = 5;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==n-1 || j==0 || j==n-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+
+        Que-8 */
+		int n=5;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				if(i==j || i == n-j+1){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
---
+++ src/test/java/com/geekster/New_Patterns/NewPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.New_Patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
